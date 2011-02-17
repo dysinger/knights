603b82c59a1d7662901f8a7cb929edf639f07567
Tim Dysinger <tim@dysinger.net>
2011-02-17 00:34:11 -0800

    Updated project build file

--- project.clj
+++ project.clj
@@ -1,3 +1,4 @@
 (defproject knights "1.0.0-SNAPSHOT"
   :description "Knights Tour in Clojure"
-  :dependencies [[org.clojure/clojure "1.3.0-alpha4"]])
+  :dependencies [[org.clojure/clojure "1.2.0"]]
+  :main knights.tour)
