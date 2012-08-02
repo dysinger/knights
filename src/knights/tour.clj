5159c903ab4c1726e9db91e0142cf39072e16f3f
Gary Fredericks <fredericksgary@gmail.com>
2012-08-01 19:52:03 -0500

    Let's get that line count down

--- src/knights/tour.clj
+++ src/knights/tour.clj
@@ -3,41 +3,31 @@
   (:use clojure.pprint))
 
 (defn board
-  "Generate a chess board as a size*size vector of zeros"
+  "Generate a chess board as a size*size vector of zeros. Use get-in
+  and assoc-in to access/update the squares."
   [size]
   (vec (repeat size (vec (repeat size 0)))))
-
-(defn square
-  "Retrieve the value of the chessboard square at [vert horz]"
-  [board [vert horz]]
-  (get (get board vert) horz))
-
-(defn update
-  "Update the chess board at [vert horz] to the move value"
-  [board [vert horz] move]
-  (assoc board vert (assoc (get board vert) horz move)))
 
 (defn moves
   "Find all the moves that can be taken from [vert horz]"
   [board [vert horz]]
-  (filter
-   (fn [p] (= 0 (square board p)))
-   (map (fn [[v h]] [(+ v vert) (+ h horz)])
-        [[ 1  2] [ 1 -2] [-1  2] [-1 -2] [ 2  1] [ 2 -1] [-2  1] [-2 -1]])))
+  (for [[v h] [[ 1  2] [ 1 -2] [-1  2] [-1 -2] [ 2  1] [ 2 -1] [-2  1] [-2 -1]]
+        :let [p [(+ v vert) (+ h horz)]]
+        :when (= 0 (get-in board p))]
+    p))
 
 (defn best
   "Find the move with the least possible moves"
   [board pos]
-  (let [m (moves board pos)]
-    (when-not (empty? m)
-      (apply min-key (fn [p] (count (moves board p))) m))))
+  (when-let [m (seq (moves board pos))]
+    (apply min-key (fn [p] (count (moves board p))) m)))
 
 (defn solve
   "Solve the knights open-tour problem"
   ([board pos count]
-     (let [new-board (update board pos count)]
+     (let [new-board (assoc-in board pos count)]
        (if-let [new-move (best board pos)]
-         (solve new-board new-move (+ 1 count))
+         (recur new-board new-move (+ 1 count))
          new-board)))
   ([] (solve (board 8) [7 0] 1)))
 
